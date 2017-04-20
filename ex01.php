1269a066ba10ebf264650cbe9744134d158e7725
anneleyder@gmail.com <anne.leyder@minfin.fed.be>
2017-04-20 14:54:16 +0200

    exercices

---
+++ ex01.php
@@ -0,0 +1,9 @@
+<?php
+$txt='';
+for ($i=1; $i < 10; $i++) {
+   $txt.=$i.'-'; 
+}
+//$txt.=$i;
+echo $i;
+echo $txt;
+?>
---
+++ ex03.php
@@ -0,0 +1,10 @@
+<?php
+$txt='';
+for ($i=1; $i < 10; $i++) {
+   $lig = '';
+   for ($j=1; $j <= i; $j++) {
+      $lig 
+   }
+}
+$txt.=$i;
+echo $txt;
